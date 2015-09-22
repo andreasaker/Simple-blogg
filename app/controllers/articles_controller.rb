a3357ff74ac40dbf15d613ce12da8df2a1ef7db4
phunlife <andreas.aker91@gmail.com>
2015-09-22 21:30:49 +0200

    Form create, edit, delete added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+	include ArticlesHelper
+
 	def index
 		@articles = Article.all
 	end
@@ -6,4 +8,39 @@ class ArticlesController < ApplicationController
 	def show
 		@article = Article.find(params[:id])
 	end
-end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		@article.save
+
+		flash.notice = "#{@article.title} was created!"
+
+		redirect_to article_path(@article)
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		redirect_to '/articles'
+		flash.notice = "Article #{@article.title} was deleted"
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+		
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+
+		flash.notice = "Article #{@article.title} updated!"
+
+		redirect_to article_path(@article)
+	end
+end 
+
